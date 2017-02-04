654a83c0a0a920acd5f8ec7d331d9449042ecbda
christianbegines <christianbeginescarrasco@gmail.com>
2017-02-04 16:56:12 +0100

    feat:estructurar y comenzar con el drag'n'drop

--- public_html/assets/js/models/burger.model.js
+++ public_html/assets/js/models/burger.model.js
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+let maxIngredientes=12;
+
 
 var Burger = function(panSup,panInf,carne){
     this.panInferior=panInf;
     this.panSuperior=panSup;
     this.carne=carne;
 };
-Burger.prototype.modelCheck=function(){
-    
+Burger.prototype.generateBurger=function(){
+   
 };
