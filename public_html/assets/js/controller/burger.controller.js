42ad2a71f7cba7722db0853fe6f13bf0531ae0eb
christianbegines <christianbeginescarrasco@gmail.com>
2017-02-04 17:57:30 +0100

    feat:siguiendo con el drag and drop

--- public_html/assets/js/controller/burger.controller.js
+++ public_html/assets/js/controller/burger.controller.js
@@ -2,30 +2,24 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */ 
+ */
 
+function generateBurger() {
+    var burger = new Burger(false, false, false);
+    var random = ((Math.random() * 1));
 
+    if (random > 0 && random <= 0.2) {
+        $('#receta-actual').replaceWith('<img id="receta-actual" src="assets/images/recetas/receta1.png"/>');
 
-$(document).ready(function(){
-  
-});
-    
-function generateBurger(){
-    var burger= new Burger(false,false,false);
-    var random=((Math.random() * 1)); 
-    
-    if(random>0 && random<=0.2){      
-        $('#receta-actual').replaceWith('<img id="receta-actual" src="assets/images/receta1.png"/>');
-        
-    }else if(random>0.2 && random<=0.5){
-       $('#receta-actual').replaceWith('<img  id="receta-actual"  src="assets/images/receta2.png"/>');
-       Object.defineProperty(burger,'tomate',{value:false});
-       Object.defineProperty(burger,'pepinillo',{value:false});
-       delete burger.carne;
-    }else{
-        $('#receta-actual').replaceWith('<img  id="receta-actual"  src="assets/images/receta3.png"/>');
-      Object.defineProperty(burger,'lechuga',{value:false});
+    } else if (random > 0.2 && random <= 0.5) {
+        $('#receta-actual').replaceWith('<img  id="receta-actual"  src="assets/images/recetas/receta2.png"/>');
+        Object.defineProperty(burger, 'tomate', {value: false});
+        Object.defineProperty(burger, 'pepinillo', {value: false});
+        delete burger.carne;
+    } else {
+        $('#receta-actual').replaceWith('<img  id="receta-actual"  src="assets/images/recetas/receta3.png"/>');
+        Object.defineProperty(burger, 'lechuga', {value: false});
     }
-    console.log(burger);
+    
 }
 
